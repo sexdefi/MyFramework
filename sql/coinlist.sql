8fb3b74869c2e83f26223b0285369203be0d8757
sexdefi <sexdefi@gmail.com>
2023-11-30 15:25:37 +0800

    费率代币黑名单

--- sql/coinlist.sql
+++ sql/coinlist.sql
@@ -7,10 +7,11 @@ create table if not exists rate_coin
     data_status   int default 1 null comment '数据状态:1，有效，0，无效',
     create_time   datetime      null comment '创建时间',
     update_time   datetime      null comment '更新时间',
+    chain_type varchar(50) null comment '链',
     constraint rate_coin_token_address_uindex
         unique (token_address)
 )
-comment '有费率的币' charset = utf8;
+    comment '有费率的币' charset = utf8;
 
 -- 菜单 SQL
 insert into sys_menu (menu_name, parent_id, order_num, url, menu_type, visible, perms, icon, create_by, create_time, update_by, update_time, remark)
